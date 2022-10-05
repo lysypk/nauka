777b1e817a6a9818cdec708e177ef2f2274fbab3
piotr.kedzierski <piotr.kedzierski9002@gmail.com>
2022-10-05 12:29:34 +0200

    scripts update

--- test/features/steps-definitions/given.ts
+++ test/features/steps-definitions/given.ts
@@ -5,9 +5,9 @@ import LoginPage from "../../page-objects/login.page"
 
 Given(/^Login to inventory app$/, async function () {
 
-    await browser.url('https://www.saucedemo.com/')
-    await browser.setTimeout({ implicit: 15000, pageLoad: 100000 })
-    await browser.maximizeWindow()
+    // await browser.url('https://www.saucedemo.com/')
+    // await browser.setTimeout({ implicit: 15000, pageLoad: 100000 })
+    // await browser.maximizeWindow()
     try {
         await LoginPage.navigateTo(browser.config.baseUrlSauce)
         await LoginPage.loginToSauce(this.testid, process.env.testUsername, process.env.testPassword)

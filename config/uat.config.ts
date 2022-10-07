6ff6b774c4e960341b35c5e977c41cc39d7bf26c
lysypk <lysypk@users.noreply.github.com>
2022-10-07 07:02:36 +0000

    Automated deployment: Fri Oct  7 07:02:36 UTC 2022 b40ec22e4adf6b5ce5e18ae16d28dc27f8ce50e6

--- config/uat.config.ts
+++
@@ -1,6 +0,0 @@
-import {config as baseConfig } from "../wdio.conf"
-export const config = Object.assign(baseConfig,{
-    //All env specific values
-    environment : "UAT",
-    baseUrlSauce: "https://www.saucedemo.com/"
-}) 

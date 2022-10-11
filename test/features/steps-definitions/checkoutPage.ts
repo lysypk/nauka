ad0f9f90d8a0869f3d4bbc806f301ebd18d2e4f2
piotr.kedzierski <piotr.kedzierski9002@gmail.com>
2022-10-11 13:03:35 +0200

    Final steps for order creation are added

--- test/features/steps-definitions/checkoutPage.ts
+++ test/features/steps-definitions/checkoutPage.ts
@@ -12,8 +12,22 @@ Given(/^User filled up address form$/, async function() {
         await CheckoutPage.addLastNameCheckout(this.testid)
         await CheckoutPage.addStreetAddressCheckout(this.testid)
         await CheckoutPage.addCityCheckout(this.testid)
+        await CheckoutPage.selectCountry(this.testid)
+        await CheckoutPage.selectState(this.testid)
+        await CheckoutPage.addZipCode(this.testid)
+        await CheckoutPage.addPhoneNumber(this.testid)
     } catch (err) {
         err.message = `Error during filling of address form on checkout ${err.message}`
+        throw err
+    }
+})
+Given(/^User selected shipping method$/, async function() {
+    logger.info(`${this.testid}: User trying to select shipping method`)
+    try {
+        await CheckoutPage.selectPaymentMethod(this.testid)
+        await CheckoutPage.goToSummaryPage(this.testid)
+    } catch (err) {
+        err.message = `Error during payment method selection: ${err.message}`
         throw err
     }
 })
--- test/page-objects/checkout.ts
+++ test/page-objects/checkout.ts
@@ -12,8 +12,8 @@ class CheckoutPage extends Page {
     get lastNameFieldCheckout() { return $('[name="lastname"]') }
     get addressFieldCheckout() { return $('[name="street[0]"]') }
     get cityFieldCheckout() { return $('[name="city"]') }
-    get countryDropdownCheckout() { return $('.country_id') }
-    get stateDropdownCheckout() { return $('.region_id') }
+    get countryDropdownCheckout() { return $('[name="country_id"') }
+    get stateDropdownCheckout() { return $('[name="region_id"]') }
     get postcodeFieldCheckout() { return $('[name="postcode"]') }
     get phoneFieldCheckout() { return $('[name="telephone"]') }
     get paymentRadioButtons() { return $$('.radio') }
@@ -45,7 +45,7 @@ class CheckoutPage extends Page {
     async addLastNameCheckout(testid : string) {
         try {
             await this.click(await this.lastNameFieldCheckout)
-            await this.addValueTo(await this.lastNameFieldCheckout, "Piotr")
+            await this.addValueTo(await this.lastNameFieldCheckout, "Kedzierski")
             reporter.addStep(testid, "info", `User try to fill last name field`)
         } catch (err) {
             err.message = `Error during adding last name, ${err.message}`
@@ -56,7 +56,7 @@ class CheckoutPage extends Page {
     async addStreetAddressCheckout(testid : string) {
         try {
             await this.click(await this.addressFieldCheckout)
-            await this.addValueTo(await this.addressFieldCheckout, "Piotr")
+            await this.addValueTo(await this.addressFieldCheckout, "Pomorska 94")
             reporter.addStep(testid, "info", `User try to fill address field`)
         } catch (err) {
             err.message = `Error during adding address, ${err.message}`
@@ -67,13 +67,69 @@ class CheckoutPage extends Page {
     async addCityCheckout(testid : string) {
         try {
             await this.click(await this.cityFieldCheckout)
-            await this.addValueTo(await this.cityFieldCheckout, "Piotr")
+            await this.addValueTo(await this.cityFieldCheckout, "Łódź")
             reporter.addStep(testid, "info", `User try to fill City field`)
         } catch (err) {
             err.message = `Error during adding city, ${err.message}`
             throw err
         }
        
+    }
+    async selectCountry(testid : string) {
+        try {
+            await this.countryDropdownCheckout.selectByVisibleText("Poland")
+            reporter.addStep(testid, "info", `User select Poland as country`)
+        } catch (err) {
+            err.message = `Error on country dropdown: ${err.message}`
+            throw err
+        }
+    }
+    async selectState(testid : string) {
+        try {
+            await this.stateDropdownCheckout.selectByVisibleText("łódzkie")
+            reporter.addStep(testid, "info", `User select łódzkie as state`)
+        } catch (err) {
+            err.message = `Error on state dropdown: ${err.message}`
+            throw err
+        }
+    }
+    async addZipCode(testid: string) {
+        try {
+            await this.click(await this.postcodeFieldCheckout)
+            await this.addValueTo(await this.postcodeFieldCheckout, "91-402")
+            reporter.addStep(testid, "info", "User is adding ZIP-Code" )
+        } catch (err) {
+            err.message = `Error during ZIP-Code add ${err.message}`
+            throw err
+        }
+    }
+    async addPhoneNumber(testid: string) {
+        try {
+            await this.click(await this.phoneFieldCheckout)
+            await this.addValueTo(await this.phoneFieldCheckout, "123123123")
+            reporter.addStep(testid, "info", "User is adding phone number" )
+        } catch (err) {
+            err.message = `Error during phone number add ${err.message}`
+            throw err
+        }
+    }
+    async selectPaymentMethod(testid: string) {
+        try {
+            await this.click(await this.paymentRadioButtons[0])
+            reporter.addStep(testid, "info", "User is selecting payment method")
+        } catch (err) {
+            err.message = `Error during selection of payment method: ${err.message}`
+            throw err
+        }
+    }
+    async goToSummaryPage(testid: string) {
+        try {
+            await this.click(await this.goToNextStepButton)
+            reporter.addStep(testid, "info", "User is clicking on go to next step button")
+        } catch (err) {
+            err.message = `Error during proceeding o checkout: ${err.message}`
+            throw err
+        }
     }
 }
 

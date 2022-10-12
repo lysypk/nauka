b61e2a5e36dc42928ac595c87b8e602b89f8d1ed
piotr.kedzierski <piotr.kedzierski9002@gmail.com>
2022-10-12 11:04:34 +0200

    changes to config added

---
+++ config/demosite_headless.config.ts
@@ -0,0 +1,23 @@
+import {config as baseConfig } from "../wdio.conf"
+export const config = Object.assign(baseConfig,{
+    //All env specific values
+    environment : "Magento Demo",
+    baseUrlSite: "https://magento.softwaretestingboard.com/",
+    capabilities: [{
+
+        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 5,
+        acceptInsecureCerts: true,
+        timeouts: { implicit: 15000, pageLoad: 20000, script: 30000 },
+        //
+        browserName: 'chrome',
+        //headless/other arg
+
+        'goog:chromeOptions': {
+            args: ["--disable-web-security", "--headless", "--disable-dev-shm-usage", "--no-sandbox", "--window-size=1920,1080"]
+        }
+
+    }]
+}) 
